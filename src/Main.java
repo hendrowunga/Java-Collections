13671bad7acd6f089ebffba929cbd310fc357c36
hendrowunga <hendrowunga073@gmail.com>
2025-01-26 23:11:25 +0700

    🚀: Release Java Collections V1.1 – Improved code examples, enhanced documentation, and added new features for better clarity and functionality. 📚✨

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner=new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("Masukan sebuah string: ");
+        String input=scanner.nextLine();
+
+        printString(input);
+        scanner.close();
+
+    }
+
+    public static void printString(String input){
+        if(input.length()>3){
+            String reversed=new StringBuilder(input).reverse().toString();
+            System.out.println("Output : "+reversed);
+        }else{
+            System.out.println("Output : "+input);
         }
     }
 }
